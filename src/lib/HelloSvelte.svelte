d147d69da99aaa1d0a898d55e8c7a890388a7ee2
abdurahmanrizal <abdurahmanrizal1@gmail.com>
2025-03-16 21:20:53 +0700

    first commit

---
+++ src/lib/HelloSvelte.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  const name = 'Svelte';
+  const src = 'https://www.programmerzamannow.com/img/pzn.png'
+</script>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
+
+<h1>Hello, {name.toUpperCase()}!</h1>
+<img {src} alt="Programmer Zaman Now" />
